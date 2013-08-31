f0047f40b039c7c8ff0196f0f9227df9ef4e2fcc
Lucky <lucky@phoenixcreatives.net>
2013-08-31 03:49:08 -0700

    Testing of threads -- passing arguments to threads

--- src/udtp.cpp
+++ src/udtp.cpp
@@ -7,9 +7,52 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <semaphore.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <time.h>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <poll.h>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+using namespace std;
+struct data{
+	int id;
+	string name;
+};
+
+void* exampleThread(void* data){
+	struct data *data_ptr = (struct data*)data;
+	cout << data_ptr ->name << " " << data_ptr->id << endl;
+	return NULL;
+}
+
+bool createThread(int counter){
+	pthread_t t1;
+	data pass_data;
+	pass_data.id = counter;
+	pass_data.name = "Thread";
+	pthread_create(&t1, NULL, exampleThread, &pass_data);
+	pthread_join(t1, NULL);
+	return true;
+}
+
+int main(int argc, char* argv) {
+	for(int i=0; i<1000; i++){
+		createThread(i);
+	}
+
 	return 0;
+
 }
